66e84decd987b1cd8a647561820988a26a702707
Devin Taylor <dntaylor@ucdavis.edu>
2019-01-21 18:23:44 -0600

    initial hzz selections

--- python/MonoHZZAnalysis.py
+++ python/MonoHZZAnalysis.py
@@ -32,6 +32,7 @@ class MonoHZZAnalysis(AnalysisBase):
         # setup cut tree
         self.cutTree.add(self.fourLoose,'fourLooseLeptons')
         self.cutTree.add(self.trigger,'trigger')
+        self.cutTree.add(self.vertex,'vertex')
 
         # setup analysis tree
 
@@ -61,6 +62,60 @@ class MonoHZZAnalysis(AnalysisBase):
     ############################
     ### select 4l candidates ###
     ############################
+    def passMuon(self,cand):
+        cuts = {
+            'pt' : lambda cand: cand.pt()>5,
+            'eta': lambda cand: abs(cand.eta())<2.4,
+            'dxy': lambda cand: abs(cand.dxy())<0.5,
+            'dz' : lambda cand: abs(cand.dz())<1,
+            # muon best track type not in nanoaod?
+            #'id' : lambda cand: (cand.isGlobal() or (cand.isTracker() and cand.nStations()>0)), # and cand.muonBestTrackType!=2,
+            # global muon not in current version of nanoaod (but is in cmssw master)
+            'id' : lambda cand: True, # isLooseMuon part of cut to include
+            # note, this needs to be done after FSR, so probably move
+            'iso': lambda cand: cand.pfRelIso03_all()<0.35,
+            'sip': lambda cand: abs(cand.sip3d())<4,
+        }
+        # TODO ghost cleaning
+        return all([cuts[cut](cand) for cut in cuts])
+
+    def passTightMuon(self,cand):
+        cuts = {
+            'loose': lambda cand: self.passMuon(cand),
+            'tight': lambda cand: cand.isPFcand() or (cand.highPtId()>0 and cand.pt()>200),
+        }
+        return all([cuts[cut](cand) for cut in cuts])
+
+    def passElectron(self,cand):
+        pt = cand.pt()
+        eta = abs(cand.eta() + cand.deltaEtaSC())
+        # not included in nanoaod yet (but is on master)
+        #mva = cand.mvaFall17V1Iso() # note, should be V2
+        cuts = {
+            'pt' : lambda cand: cand.pt()>7,
+            'eta': lambda cand: abs(cand.eta())<2.5,
+            'dxy': lambda cand: abs(cand.dxy())<0.5,
+            'dz' : lambda cand: abs(cand.dz())<1,
+            'id' : lambda cand: cand.mvaFall17Iso_WPL()>0.5, # tmp
+            #'id' : lambda cand: (pt<=10 and ((eta<0.8                and mva>0.5739521065342641)\
+            #                            or   (eta>=0.8 and eta<1.479 and mva>0.5504628790992929)\
+            #                            or   (eta>=1.479             and mva>0.5924627534389098)))\
+            #                    or\
+            #                    (pt>10  and ((eta<0.8                and mva>-0.03391387993354392)\
+            #                            or   (eta>=0.8 and eta<1.479 and mva>-0.018451958064666783)\
+            #                            or   (eta>=1.479             and mva>-0.38565459150737535))),
+            'iso': lambda cand: cand.pfRelIso03_all()<0.35,
+            'sip': lambda cand: abs(cand.sip3d())<4,
+        }
+        return all([cuts[cut](cand) for cut in cuts])
+
+    def passPhoton(self,cand):
+        # note, not possible given the pt cuts in nanoaod (5 vs 2 needed)
+        return True
+
+    def passJet(self,cand):
+        return True
+
     def selectCandidates(self):
         candidate = {
             'z11' : None,
@@ -75,9 +130,17 @@ class MonoHZZAnalysis(AnalysisBase):
         }
 
         # get leptons
-        leps = self.electrons+self.muons
+        muons = self.getCands(self.muons,self.passMuon)
+        electrons = self.getCands(self.electrons,self.passElectron)
+
+        # cross clean electrons
+        tightmuons = self.getCands(self.muons,self.passTightMuon)
+        electrons = self.cleanCands(electrons,tightmuons,0.05)
+
+        leps = electrons + muons
         if len(leps)<4: return candidate # need at least 4 leptons
 
+        # TODO FSR not added
 
         # get the candidates
         zzCands = []
@@ -88,21 +151,45 @@ class MonoHZZAnalysis(AnalysisBase):
             # require same type
             if quad[0].collName!=quad[1].collName: continue
             if quad[2].collName!=quad[3].collName: continue
-            # require deltaR seperation of 0.02 m(ll)>12
+            # require hzz cuts
+            z1 = DiCandidate(quad[0],quad[1])
+            m1 = z1.M()
+            z2 = DiCandidate(quad[2],quad[3])
+            m2 = z2.M()
+            if m1<40 or m1>120 or m2<12 or m2>120: continue
+            # combinatoric cuts
             keep = True
             for i,j in itertools.combinations(range(4),2):
                 dicand = DiCandidate(quad[i],quad[j])
                 dr = dicand.deltaR()
                 m = dicand.M()
                 if dr<0.02: keep = False
+                if quad[i].charge()+quad[j].charge()==0 and m<4.: keep = False
             if not keep: continue
-            # require lead e/m pt > 25
-            ems = [cand for cand in quad if cand.collName in ['electrons','muons']]
+            # pt cuts
+            pts = sorted([quad[i].pt() for i in range(4)],reverse=True)
+            if pts[0]<20: continue
+            if pts[1]<10: continue
+            # smart cut
+            if quad[0].collName==quad[2].collName:
+                if quad[0].charge()!=quad[2].charge():
+                   pairs = [[0,2],[1,3]]
+                else:
+                   pairs = [[0,3],[1,2]]
+                zX = DiCandidate(*[quad[i] for i in pairs[0]])
+                zY = DiCandidate(*[quad[i] for i in pairs[1]])
+                za = zX if abs(zX.M()-ZMASS) < abs(zY.M()-ZMASS) else zY
+                zb = zY if abs(zX.M()-ZMASS) < abs(zY.M()-ZMASS) else zX
+                if abs(za.M()-ZMASS)<abs(z1.M()-ZMASS) and zb.M()<12: continue
+            # 4 body cut
+            zz = CompositeCandidate(*quad)
+            if zz.M()<70: continue
             # its a good candidate
             zzCands += [quad]
         if not zzCands: return candidate
 
         # sort by best z then st
+        # note: should be selected by P_sig/P_bkg (but dont know what this is)
         bestZ = 0
         highestSt = 0
         bestCand = []
@@ -152,6 +239,10 @@ class MonoHZZAnalysis(AnalysisBase):
     ###########################
     def fourLoose(self,cands):
         return len(self.electrons+self.muons)>=4
+
+    def vertex(self,cands):
+        # !isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2
+        return self.event.PV_npvsGood()
 
     def trigger(self,cands):
         # accept MC, check trigger for data
@@ -218,6 +309,7 @@ class MonoHZZAnalysis(AnalysisBase):
         return False
 
 
+    
 
 
 
@@ -227,7 +319,7 @@ def parse_command_line(argv):
 
     #parser.add_argument('--inputFiles', type=str, nargs='*', default=getTestFiles('hzz'), help='Input files')
     #parser.add_argument('--inputFiles', type=str, nargs='*', default=getTestFiles('data'), help='Input files')
-    parser.add_argument('--inputFiles', type=str, nargs='*', default='doublemuon.root', help='Input files')
+    parser.add_argument('--inputFiles', type=str, nargs='*', default='hzz.root', help='Input files')
     parser.add_argument('--inputFileList', type=str, default='', help='Input file list')
     parser.add_argument('--outputFile', type=str, default='monoHZZTree.root', help='Output file')
     parser.add_argument('--shift', type=str, default='', choices=['','ElectronEnUp','ElectronEnDown','MuonEnUp','MuonEnDown','TauEnUp','TauEnDown','JetEnUp','JetEnDown','JetResUp','JetResDown','UnclusteredEnUp','UnclusteredEnDown'], help='Energy shift')
